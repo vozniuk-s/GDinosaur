2cad8c762a577c9ad1e55640c8e48fa118bf932e
dedovskiy <vozniuk.s7@gmail.com>
2025-02-28 20:01:08 +0200

    Added localization
    Added main menu localization

---
+++ Assets/Scripts/LanguageSwitcher.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Localization.Settings;
+
+public class LanguageSwitcher : MonoBehaviour
+{
+    private string currentLanguage = "en";
+
+    private void Start()
+    {
+        SwitchLanguage();
+    }
+
+    public void SwitchLanguage()
+    {
+        currentLanguage = (currentLanguage == "en") ? "uk" : "en";
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.GetLocale(currentLanguage);
+        Debug.Log(currentLanguage);
+    }
+}
